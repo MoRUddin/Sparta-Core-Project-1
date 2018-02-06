73437b33c1b57faaac3ac22aa5eedf1e09f0d545
Tech-A22 <tech-a22@Tech-A22s-MacBook-Air.local>
2018-02-06 14:45:00 +0000

    win condition created

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,47 @@
-var isDown = false;
-var totalTurns = 0;
-
 $(document).ready(function() {
-  $('#red-block').draggable(
+  var isDown = false;
+  var totalTurns = 0;
+  var $turnCounter = $('#turnCounter');
+
+  $('.goal').offset({top:28,left:609});
+  $('.orangeBlockVertical').offset({top:28,left:309});
+
+  $('#redBlock').draggable(
     {
       containment:'parent',
-      cursor:'none',
-      axis: 'x'
-    });
+      axis: 'x',
+      grid: [150,150],
+      drag: function(){
+      },
+      stop: function() {
+        turnIncrement();
+        if(this.offsetLeft==602&&this.offsetTop==2) {
+          alert('winRaR');
+        }
+      }
+  });
+  var $orangeBlockVertical = $('.orangeBlockVertical');
+  $orangeBlockVertical.draggable(
+    {
+      containment:'parent',
+      axis: 'y',
+      grid: [150,150],
+      drag: function(){
+      },
+      stop: turnIncrement
+  });
+  var $orangeBlockHorizontal = $('.orangeBlockHorizontal');
+  $orangeBlockHorizontal.draggable(
+    {
+      containment:'parent',
+      axis: 'x',
+      grid: [150,150],
+      stop: turnIncrement
+  });
+
+  function turnIncrement(){
+    totalTurns++;
+    $turnCounter.html('Turns: '+totalTurns);
+  }
+
 });
