5876aef2f6bdad88bdb93c1c05f9a8624e91b2a9
Tech-A22 <tech-a22@Tech-A22s-MacBook-Air.local>
2018-02-07 16:41:49 +0000

    complete overhaul, -removed draggable, -currently implementing block movement and then collisions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,84 +1,29 @@
 $(document).ready(function() {
-  var isDown = false;
-  var totalTurns = 0;
+  var $mdir = $.mousedirection();
+  var $block = $('.block');
   var $turnCounter = $('#turnCounter');
 
-  $('.goal').offset({top:28,left:609});
-  $('.orangeBlockVertical').offset({top:28,left:309});
-
-  $('#redBlock').draggable(
-    {
-      containment:'parent',
-      axis: 'x',
-      grid: [150,150],
-      drag: function(){
-        if(collisionDetect()){
-          console.log('collision');
-          $("#redBlock").draggable( "option", "grid",[0,0]);
-        } else {
-          console.log('no collision');
-        }
-      },
-      stop: function() {
-        turnIncrement();
-        if(this.offsetLeft==602&&this.offsetTop==2) {
-          alert('winRaR');
-        }
-      }
-  });
-  var $orangeBlockVertical = $('.orangeBlockVertical');
-  $orangeBlockVertical.draggable(
-    {
-      containment:'parent',
-      axis: 'y',
-      grid: [150,150],
-      drag: function(){
-        if(collisionDetect()){
-          $(".selector").draggable("option", "grid",[0,0]);
-        } else {
-          console.log('no collision');
-        }
-      },
-      stop: turnIncrement
-  });
-  var $orangeBlockHorizontal = $('.orangeBlockHorizontal');
-  $orangeBlockHorizontal.draggable(
-    {
-      containment:'parent',
-      axis: 'x',
-      grid: [150,150],
-      drag: function(){
-        if(collisionDetect()){
-          $(".selector").draggable( "option", "grid",[0,0]);
-        } else {
-          console.log('no collision');
-        }
-      },
-      stop: turnIncrement
+  //update mouse direction
+  $(".gcBody").bind("mousedirection", function (e) {
+      $mdir = e.direction;
+      console.log($mdir);
   });
 
-  function turnIncrement(){
-    totalTurns++;
-    $turnCounter.html('Turns: '+totalTurns);
-  }
-  function collisionDetect(){
-    if(topRightCollision()||bottomRightCollision()||bottomLeftCollision()||topLeftCollision()){
-      return true;
-    } else {
-      return false;
+  //make all blocks clickable
+  $block.on('mousedown', function() {
+    switch ($(this).hasClass('horz')) {//detects whether the blocks should move horizontally or vertically
+      case true:
+          //$(this).css('left', this.offsetLeft+30);
+          $(this).on('mousedirection'){
+
+          }
+        break;
+      case false:
+          console.log('working');
+        break;
+      default:
+
     }
-  }
-  function topRightCollision() {
-    return false;
-  }
-  function bottomRightCollision() {
-    return false;
-  }
-  function bottomLeftCollision() {
-    return false;
-  }
-  function topLeftCollision() {
-    return false;
-  }
+  });
 
 });
